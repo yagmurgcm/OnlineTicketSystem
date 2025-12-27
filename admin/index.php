26827b3500c893ba6ae4b629aa2c3d3989a97f45
Yagmur Gecim <yagmur.gecim@sabanciuniv.edu>
2025-12-27 17:46:14 +0300

    Add admin panel skeleton

---
+++ admin/index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Admin Panel</h1>";
+echo "<ul>";
+echo "<li><a href='tickets/'>View Support Tickets</a></li>";
+echo "<li><a href='../user/index.php'>Go to User Page</a></li>";
+echo "</ul>";
+
--- user/index.php
+++ user/index.php
@@ -5,4 +5,5 @@ echo "<li><a href='triggers/'>Triggers</a></li>";
 echo "<li><a href='procedures/'>Stored Procedures</a></li>";
 echo "<li><a href='tickets/'>Support Tickets</a></li>";
 echo "</ul>";
+echo "<br><a href='../admin/index.php'>Admin Panel</a>";
 
