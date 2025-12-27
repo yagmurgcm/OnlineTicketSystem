13b872c6e300c2c63eef8cd598562160985d98ad
Yagmur Gecim <yagmur.gecim@sabanciuniv.edu>
2025-12-27 17:44:54 +0300

    Initial project structure and user homepage

---
+++ user/index.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h1>Online Ticket System - CS306 Phase 3</h1>";
+echo "<ul>";
+echo "<li><a href='triggers/'>Triggers</a></li>";
+echo "<li><a href='procedures/'>Stored Procedures</a></li>";
+echo "<li><a href='tickets/'>Support Tickets</a></li>";
+echo "</ul>";
+
